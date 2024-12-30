88a14dedb9d466d4e668e4b5f61759a7c857c4fd
masonkimseoul <masonkimseoul@gmail.com>
2024-12-30 14:22:50 +0900

    refactor: PaperDetailResponse에 boardId 추가

--- src/main/java/com/zollingpaper/backend/paper/dto/PaperDetailResponse.java
+++ src/main/java/com/zollingpaper/backend/paper/dto/PaperDetailResponse.java
@@ -3,9 +3,13 @@ package com.zollingpaper.backend.paper.dto;
 import com.zollingpaper.backend.paper.domain.Paper;
 import java.time.LocalDateTime;
 
-public record PaperDetailResponse(Long paperId, LocalDateTime createdAt, String name, String content) {
+public record PaperDetailResponse(Long id, Long boardId, LocalDateTime createdAt, String name, String content) {
 
     public static PaperDetailResponse from(Paper paper) {
-        return new PaperDetailResponse(paper.getId(), paper.getCreatedAt(), paper.getName(), paper.getContent());
+        return new PaperDetailResponse(paper.getId(),
+                paper.getBoard().getId(),
+                paper.getCreatedAt(),
+                paper.getName(),
+                paper.getContent());
     }
 }
