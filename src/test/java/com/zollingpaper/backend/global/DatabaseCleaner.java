9673e2b92b234f91ff965ce50a458fedf986fe1e
masonkimseoul <masonkimseoul@gmail.com>
2024-12-31 14:44:19 +0900

    refactor: DatabaseCleaner 중복 메서드 제거

--- src/test/java/com/zollingpaper/backend/global/DatabaseCleaner.java
+++ src/test/java/com/zollingpaper/backend/global/DatabaseCleaner.java
@@ -2,6 +2,7 @@ package com.zollingpaper.backend.global;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,18 +15,18 @@ public class DatabaseCleaner {
 
     public void execute() {
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
-        clearPaperEntity();
-        clearBoardEntity();
+        getTruncateQueries().forEach(query -> entityManager.createNativeQuery(query).executeUpdate());
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
     }
 
-    private void clearPaperEntity() {
-        entityManager.createNativeQuery("DELETE FROM PAPER").executeUpdate();
-        entityManager.createNativeQuery("ALTER TABLE PAPER ALTER COLUMN id RESTART").executeUpdate();
-    }
+    private List<String> getTruncateQueries() {
+        String sql = """
+                SELECT CONCAT('TRUNCATE TABLE ', TABLE_NAME, ' RESTART IDENTITY',';')
+                FROM INFORMATION_SCHEMA.TABLES
+                WHERE TABLE_SCHEMA = 'PUBLIC'
+                """;
 
-    private void clearBoardEntity() {
-        entityManager.createNativeQuery("DELETE FROM BOARD").executeUpdate();
-        entityManager.createNativeQuery("ALTER TABLE BOARD ALTER COLUMN id RESTART").executeUpdate();
+        return entityManager.createNativeQuery(sql)
+                .getResultList();
     }
 }
