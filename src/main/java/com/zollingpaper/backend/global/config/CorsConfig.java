6166ccda2064d26f8bf8cf67314e44823bd04fdc
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2025-02-06 17:06:00 +0900

    feat: CORS Config 구성 (#15)

---
+++ src/main/java/com/zollingpaper/backend/global/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.zollingpaper.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    public CorsConfig() {
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://zolling.me/", "https://api.zolling.me/", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
