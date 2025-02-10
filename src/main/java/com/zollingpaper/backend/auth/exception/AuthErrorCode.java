b2bdc54931946668a518258b7ac88576e4854e39
masonkimseoul <masonkimseoul@gmail.com>
2025-02-10 18:26:16 +0900

    feat: 인증 관련 Exception 구현

---
+++ src/main/java/com/zollingpaper/backend/auth/exception/AuthErrorCode.java
@@ -0,0 +1,28 @@
+package com.zollingpaper.backend.auth.exception;
+
+import com.zollingpaper.backend.global.exception.ErrorMessage;
+import com.zollingpaper.backend.global.exception.ErrorResponse;
+import org.springframework.http.HttpStatus;
+
+public enum AuthErrorCode implements ErrorResponse {
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    AuthErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public ErrorMessage getErrorMessage() {
+        return new ErrorMessage(this.message);
+    }
+}
