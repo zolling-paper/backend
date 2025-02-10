170903dad553e13ab14379e15281cd0d8311343e
masonkimseoul <masonkimseoul@gmail.com>
2025-02-10 18:26:35 +0900

    feat: AuthController 구현

---
+++ src/main/java/com/zollingpaper/backend/auth/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.zollingpaper.backend.auth.controller;
+
+import com.zollingpaper.backend.auth.service.AuthService;
+import com.zollingpaper.backend.auth.dto.LoginRequest;
+import com.zollingpaper.backend.auth.dto.LoginResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody LoginRequest request
+    ) {
+        LoginResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
