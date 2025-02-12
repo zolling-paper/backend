80e138170259f93162f66e6c2cbbf58f5cc090f2
masonkimseoul <masonkimseoul@gmail.com>
2025-02-12 20:30:43 +0900

    feat: Login API에 Swagger 어노테이션 부착

--- src/main/java/com/zollingpaper/backend/auth/controller/AuthController.java
+++ src/main/java/com/zollingpaper/backend/auth/controller/AuthController.java
@@ -4,12 +4,15 @@ import com.zollingpaper.backend.auth.service.AuthService;
 import com.zollingpaper.backend.auth.dto.LoginRequest;
 import com.zollingpaper.backend.auth.dto.LoginResponse;
 import com.zollingpaper.backend.auth.util.JwtCookieProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "auth API")
 @RestController
 public class AuthController {
 
@@ -19,6 +22,7 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(summary = "login API", description = "board의 accessAddress, password로 로그인합니다.")
     @PostMapping("/login")
     public ResponseEntity<Void> login(
             @RequestBody LoginRequest request,
