8a3245f7e392bab6bfef423cd9b614992df8e254
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2025-02-18 15:58:22 +0900

    [HOT FIX] 브라우저에 쿠키가 저장되지 않는 이슈 해결 (#41)
    * fix: .domain, .sameSite 추가
    
    * debug: 테스트용 cd 트리거 수정
    
    * debug: 테스트용 cd 트리거 삭제

--- src/main/java/com/zollingpaper/backend/auth/util/JwtCookieProvider.java
+++ src/main/java/com/zollingpaper/backend/auth/util/JwtCookieProvider.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class JwtCookieProvider {
 
     private static final String COOKIE_NAME = "token";
+    private static final String DOMAIN = ".zolling.me";
     private static final int COOKIE_MAX_AGE = 1800;
 
     public static void setTokenCookie(HttpServletResponse response, String token) {
@@ -16,7 +17,9 @@ public class JwtCookieProvider {
                 .maxAge(COOKIE_MAX_AGE)
                 .httpOnly(true)
                 .secure(true)
+                .domain(DOMAIN)
                 .path("/")
+                .sameSite("None")
                 .build();
 
         response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
